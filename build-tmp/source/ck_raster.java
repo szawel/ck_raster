1eeb244cb05a9351bd334064d50820935871acf5
szawel <na.adress@gmail.com>
2014-08-13 18:24:13 +0200

    test

---
+++ build-tmp/source/ck_raster.java
@@ -0,0 +1,38 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class ck_raster extends PApplet {
+
+// m_ratser - ck raster generator //
+// generator rastra //
+
+//gui setup
+float bg_color = 255;
+
+public void setup() {
+	size(displayWidth, displayHeight);
+}
+
+public void draw() {
+	background(bg_color);
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "ck_raster" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
---
+++ ck_raster.pde
@@ -0,0 +1,13 @@
+// m_ratser - ck raster generator //
+// generator rastra //
+
+//gui setup
+float bg_color = 255;
+
+void setup() {
+	size(displayWidth, displayHeight);
+}
+
+void draw() {
+	background(bg_color);
+}
